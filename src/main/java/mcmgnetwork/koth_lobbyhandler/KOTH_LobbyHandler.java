712ad3d01125d2a3c3a16dd53eba5ce201c9b7ab
mbovero <milesbovero@icloud.com>
2024-05-06 19:32:23 -0600

    Initial commit

---
+++ src/main/java/mcmgnetwork/koth_lobbyhandler/KOTH_LobbyHandler.java
@@ -0,0 +1,17 @@
+package mcmgnetwork.koth_lobbyhandler;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KOTH_LobbyHandler extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
